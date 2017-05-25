7ee5999cde53b318591e406e216330bc7c5ea1f4
wittymoniker <bagels105oranges@gmail.com>
2017-05-25 19:14:40 -0400

    yep

--- WaveBricks/entities.cpp
+++ WaveBricks/entities.cpp
@@ -1,9 +1,5 @@
 //entities
-#ifdef __APPLE__
-#include <GLUT/glut.h>
-#else
-#include <GL/glut.h>
-#endif
+
 #include <cmath>
 #include <vector>
 #include <windows.h>
@@ -12,34 +8,11 @@
 using namespace std;
 static int slices = 128;//if needed
 static int stacks = 128;//if needed
-static void display(void)
-{
-    const double timeScale = glutGet(GLUT_ELAPSED_TIME) / 420.00;
-    const double a = timeScale*90.0;
+class instrument{
+    public:
+        float yRes, xRes, siz, x,y,z, r,g,b, rx,rrx,ry,rry,rz,rrz, fade;
+        bool gravity, collide;
+        string type;
+};
+std::vector<instrument> objList;
 
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glColor3d(1,0,0);
-    //character values can be stacked in between push and pulls with variables. once we get a general environment and character tracking we'll be set.
-    glPushMatrix();
-        glTranslated(-2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidSphere(1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(0,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidCone(1,1,slices,stacks);
-    glPopMatrix();
-
-    glPushMatrix();
-        glTranslated(2.4,1.2,-6);
-        glRotated(60,1,0,0);
-        glRotated(a,0,0,1);
-        glutSolidTorus(0.2,0.8,slices,stacks);
-    glPopMatrix();
-
-    glutSwapBuffers();
-}
--- WaveBricks/main.cpp
+++ WaveBricks/main.cpp
@@ -12,8 +12,10 @@ TODO:
 
 #ifdef __APPLE__
 #include <GLUT/glut.h>
+#include "GLUI/glui.h"
 #else
 #include <GL/glut.h>
+#include "GL/glui.h"
 #endif
 #include <cmath>
 #include <vector>
@@ -168,7 +170,37 @@ const GLfloat mat_specular[]   = { 1.0f, 1.0f, 1.0f, 1.0f };
 const GLfloat high_shininess[] = { 100.0f };
 
 /* Program entry point */
+void display(void)
+{
+    const double timeScale = glutGet(GLUT_ELAPSED_TIME) / 420.00;
+    const double a = timeScale*90.0;
 
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    glColor3d(1,0,0);
+    //character values can be stacked in between push and pulls with variables. once we get a general environment and character tracking we'll be set.
+    glPushMatrix();
+        glTranslated(-2.4,1.2,-6);
+        glRotated(60,1,0,0);
+        glRotated(a,0,0,1);
+        glutSolidSphere(1,slices,stacks);
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(0,1.2,-6);
+        glRotated(60,1,0,0);
+        glRotated(a,0,0,1);
+        glutSolidCone(1,1,slices,stacks);
+    glPopMatrix();
+
+    glPushMatrix();
+        glTranslated(2.4,1.2,-6);
+        glRotated(60,1,0,0);
+        glRotated(a,0,0,1);
+        glutSolidTorus(0.2,0.8,slices,stacks);
+    glPopMatrix();
+
+    glutSwapBuffers();
+}
 int main(int argc, char *argv[])
 {
 
